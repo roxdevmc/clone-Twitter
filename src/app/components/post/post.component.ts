3249ffe2896c897afb2797c70ce14a02647a6a9d
roxdevmc <helloworld.roxdev@gmail.com>
2023-04-14 11:11:21 -0300

    projeto angular

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+
+export class PostComponent {
+// classe que vai deixar o 240 animado, contador de caracteres
+  tweetMessage: string = "" 
+  tweetMaxLenght: number = 240
+  tweetRemain: number = this.tweetMaxLenght
+
+  sendTweet(){
+    alert(this.tweetMessage)
+  }
+
+  changeTweet(){
+    this.tweetRemain = 240 - this.tweetMessage.length
+  }
+}
